db2b69db2576be5f756bf6edd8986da67fef136b
Sebastian Vitug <svitug@scu.edu>
2020-02-26 13:50:02 -0800

    added reservation and device tables

--- blackjack/app/models.py
+++ blackjack/app/models.py
@@ -3,8 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from app import login
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 import random
-#test
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,25 +13,25 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    #hand = db.relationship('Hand', uselist=False, backref='player') #one-to-one relationship
+    reservations = db.relationship('Reservation', backref='reserver', lazy= 'dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
-    	self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-    	return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)
 
 
 class Card:
     suits = ['Spades', 'Hearts','Diamonds','Clubs']
-    ranks = [str(n) for n in range (2,11)]+['J','Q','K','A']
+    ranks = [str(n) for n in range(2,11)]+['J','Q','K','A']
 
-    def __init__(self,suit,cardvalue):
+    def __init__(self, suit,cardvalue):
         if suit.title() not in self.suits:
-            raise ValueError("'{}' is not a valid suit. Valid suits are -- {} -- passed in as a string".format(suit,self.suits))
+            raise ValueError("'{}' is not a valid suit. Valid suits are -- {} -- passed in as a string".format(suit, self.suits))
         if cardvalue.upper() not in self.ranks:
             raise ValueError("'{}' is not a valid card value. Valid suits are -- {} -- passed in as a string".format(cardvalue, self.rank))
         self.rank = cardvalue.upper()
@@ -55,6 +56,7 @@ class Card:
     def __repr__(self):
         return 'Card {} of {}'.format(self.rank, self.suit)
 
+
 class Deck:
     def __init__(self):
         self.loadnewdeck()
@@ -64,7 +66,7 @@ class Deck:
         self.deck = list()
         for rank in Card.ranks:
             for suit in Card.suits:
-                self.deck.append(Card(suit,rank))
+                self.deck.append(Card(suit, rank))
 
     def shuffledeck(self):
         random.shuffle(self.deck)
@@ -79,29 +81,28 @@ class Deck:
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, default=func.now())
+    end_time = db.Column(db.DateTime, default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    device_id = db.Column(db.Integer, db.ForeignKey('device.id'))
+
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_comments = db.Column(db.String(140))
+    reservations = db.relationship('Reservation', backref='reserved_device', lazy='dynamic')
+
+
 @login.user_loader
 def load_user(id):
-	return User.query.get(int(id))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return User.query.get(int(id))
 
